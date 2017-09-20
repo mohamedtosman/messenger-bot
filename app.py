46f39e95b6a4c8ac053c8b0e8754f9bbd138d1c7
mohamedtosman <mohamedtosman@cmail.carleton.ca>
2017-09-20 17:11:21 -0400

    trying to fix bug...

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def send_message(token, recipient, text):
     }),
     headers={'Content-type': 'application/json'})
     if r.status_code != requests.codes.ok:
-        print r.text
+        print(r.text)
 
 if __name__ == '__main__':
     app.run()
