ba79ea83ebfb4330b97e90270196c8c47aa00fba
mohamedtosman <mohamedtosman@cmail.carleton.ca>
2017-09-21 15:56:40 -0400

    adding location capability

--- app.py
+++ app.py
@@ -76,9 +76,9 @@ def send_weather():
     r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=Ottawa,Canada&APPID=facf3a7876343295f70bb6b943e3452c')
     json_obj = r.json()
     temp_k = float(json_obj['main']['temp'])
-    temp_c = temp_k - 273.15
+    temp_c = float("{0:.2f}".format(temp_k - 273.15))
 
-    return str(temp_c)
+    return temp_c
 
 def send_message(token, recipient, text):
 
